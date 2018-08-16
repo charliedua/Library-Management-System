97840a3bfcd15492b98bb09d8ac88b54116737c8
Anmol Dua <101983924@student.swin.edu.au>
2018-08-16 20:02:27 +1000

    made a fix cpu for debugging and release version. and added library item class

--- Library/LibraryItem.cs
+++ Library/LibraryItem.cs
@@ -5,11 +5,43 @@ using System.Text;
 
 namespace Library
 {
+    /// <summary>
+    /// The base Class for almost everything in this library.
+    /// </summary>
     public abstract class LibraryItem
     {
+        private bool _acquired;
+
         public LibraryItem(string name)
         {
             Name = name;
+        }
+
+        public bool Take()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// To return or release the resource captured
+        /// </summary>
+        /// <returns>
+        /// the status is it was possible or not
+        /// </returns>
+        public bool Give()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// To or acquire the resource required.
+        /// </summary>
+        /// <returns>
+        /// the status is it was possible or not.
+        /// </returns>
+        public bool IsAvailable()
+        {
+            return !_acquired;
         }
 
         /// <summary>
@@ -17,17 +49,5 @@ namespace Library
         /// </summary>
         /// <value>name of the item</value>
         public string Name { get; set; }
-
-        /// <summary>
-        /// for checking if the item has been issued or not
-        /// </summary>
-        /// <value>issued status</value>
-        public bool Booked
-        {
-            get => default(int);
-            set
-            {
-            }
-        }
     }
 }
