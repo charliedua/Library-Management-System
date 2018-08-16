82af47db47053e95c56294c7d336d9c0393ac12e
Anmol Dua <101983924@student.swin.edu.au>
2018-08-16 19:34:52 +1000

    added a class diagram and made a new class library

---
+++ Library/LibraryItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Library
+{
+    public abstract class LibraryItem
+    {
+        public LibraryItem(string name)
+        {
+            Name = name;
+        }
+
+        /// <summary>
+        /// this represents the name for the item
+        /// </summary>
+        /// <value>name of the item</value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// for checking if the item has been issued or not
+        /// </summary>
+        /// <value>issued status</value>
+        public bool Booked
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+    }
+}
